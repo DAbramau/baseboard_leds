993c461fa7ab94a7dcfe43748e474613018be239
dabramau <d.abramau@protonmail.com>
2021-10-19 22:39:51 +0300

    replace Adafruit_NeoPixel with NeoPixelBus

--- src/neo_led.cpp
+++ src/neo_led.cpp
@@ -2,20 +2,19 @@
 
 #include <Arduino.h>
 
-#include "Adafruit_NeoPixel.h"
+#include "NeoPixelBus.h"
 
 #include "utils.h"
 
 /* -------------------------------------------------------------------------- */
 
-static Adafruit_NeoPixel strip_0(CONFIG_NEOPIXEL_CH0_NUM, CONFIG_NEOPIXEL_CH0_PIN, NEO_GRB + NEO_KHZ800);
+static NeoPixelBus<NeoGrbFeature, NeoWs2813Method> strip_0(CONFIG_NEOPIXEL_CH0_NUM, CONFIG_NEOPIXEL_CH0_PIN);
 
-static uint32_t _color_list[] = 
+static RgbColor _color_list[] = 
 {
-    strip_0.Color(127, 127, 127),
-    strip_0.Color(127, 0, 0),
-    strip_0.Color(0, 127, 0),
-    strip_0.Color(0, 0, 127)
+    RgbColor(127, 0,0),
+    RgbColor(0, 127, 0),
+    RgbColor(0, 0, 127)
 };
 
 static uint32_t _mode_index = 0;
@@ -38,7 +37,7 @@ static void _mode_1(void)
 
     for (uint32_t i = 0; i < CONFIG_NEOPIXEL_CH0_NUM; ++i)
     {
-        strip_0.setPixelColor(i, i < frame_index ? _color_list[color_index] : 0);
+        strip_0.SetPixelColor(i, i < frame_index ? _color_list[color_index] : 0);
     }
 
     ++step;
@@ -54,7 +53,7 @@ static void _mode_2(void)
 {
     for (uint32_t i = 0; i < CONFIG_NEOPIXEL_CH0_NUM; ++i)
     {
-        strip_0.setPixelColor(i, _color_list[rand() % ARRAY_SIZE(_color_list)]);
+        strip_0.SetPixelColor(i, _color_list[rand() % ARRAY_SIZE(_color_list)]);
     }
 }
 
@@ -63,9 +62,9 @@ static void _mode_2(void)
 static void _mode_3(void)
 {
     static uint32_t step = 0;
-    static uint32_t color = 0;
+    static RgbColor color = 0;
 
-    static uint32_t buffer[CONFIG_NEOPIXEL_CH0_NUM] = {0};
+    static RgbColor buffer[CONFIG_NEOPIXEL_CH0_NUM];
 
     for (uint32_t i = CONFIG_NEOPIXEL_CH0_NUM - 1; i > 0; --i)
     {
@@ -74,7 +73,7 @@ static void _mode_3(void)
 
     if (step == 0)
     {
-        color = strip_0.Color(rand() % 256, rand() % 256 , rand() % 256);
+        color = RgbColor(rand() % 256, rand() % 256 , rand() % 256);
     }
 
     if (step < 2)
@@ -88,7 +87,7 @@ static void _mode_3(void)
 
     for (uint32_t i = 0; i < CONFIG_NEOPIXEL_CH0_NUM; ++i)
     {
-        strip_0.setPixelColor(i, buffer[i]);
+        strip_0.SetPixelColor(i, buffer[i]);
     }
 
     ++step;
@@ -114,7 +113,7 @@ static void neo_led_task(void* parameter)
     for (;;)
     {
         _modes[_mode_index]();
-        strip_0.show();
+        strip_0.Show();
 
         delay(100);
     }
@@ -124,9 +123,8 @@ static void neo_led_task(void* parameter)
 
 void neo_led_init(void)
 {
-    strip_0.begin();
-    strip_0.show();
-    strip_0.setBrightness(255);
+    strip_0.Begin();
+    strip_0.Show();
 
     xTaskCreate(neo_led_task,
                 "neo_led",
